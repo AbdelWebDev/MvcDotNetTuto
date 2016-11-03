c42286182ffdd5bc74face1ddc9c434c2c6473dd
AbdelWebDev <abdelouhabamri@hotmail.com>
2016-11-03 11:10:49 +0100

    first commit

---
+++ Tutoriel/Controllers/ArticleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutoriel.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        [Route("lire/{id}/{title}")]
+        public ActionResult Lire(int id,String title)
+        {
+            //String title = Request.Params["title"];
+            ViewBag.ArticleId = id;
+            if(!string.IsNullOrEmpty(title)) ViewBag.ArticleTitle = title.Replace('-',' ');
+            return View();
+        }
+
+        //[Route("all"),Route("tous")]
+        [Route("{Type:regex(all|tous)}")]
+        public ActionResult allArticles()
+        {
+            return View();
+        }
+    }
+}
